4ad1adc81df669be40750fe2bf383fa2b7fcf3d6
SamuelRicardos <samuelric4rdo@gmail.com>
2025-02-24 07:39:31 -0300

    criacao de tabelas no rds da Aamzon e insercao de valores

--- dags/dag.py
+++ dags/dag.py
@@ -3,6 +3,9 @@ from airflow.operators.python import PythonOperator
 from airflow import DAG
 from airflow.utils.task_group import TaskGroup
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+import pandas as pd
+import unidecode
 
 from scripts.captura_dados import captura_dados
 from scripts.processamento import processar_dados
@@ -14,15 +17,78 @@ def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
     hook = S3Hook('s3_conn')
     hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
 
+def formatar_nome_coluna(col):
+    col = unidecode.unidecode(col)
+    
+    col_adjusted = col.replace(" de ", "_").replace(" em ", "_").replace(" para ", "_").replace(" da ", "_").replace(" do ", "_")
+    
+    col_adjusted = col_adjusted.replace(" ", "_")
+    
+    col_adjusted = ''.join(c if c.isalnum() or c == "_" else "_" for c in col_adjusted)
+    
+    col_adjusted = col_adjusted.strip('_')
+
+    return col_adjusted
+
+def criar_tabela_sql(df: pd.DataFrame, tabela: str, hook: MsSqlHook) -> None:
+    create_table_query = f"CREATE TABLE [{tabela}] ("
+
+    for col, dtype in zip(df.columns, df.dtypes):
+
+        col_adjusted = formatar_nome_coluna(col)
+        
+        col_escaped = f"[{col_adjusted}]"
+        
+        if dtype == 'object':
+            column_type = 'VARCHAR(MAX)'
+        elif dtype == 'int64':
+            column_type = 'INT'
+        elif dtype == 'float64':
+            column_type = 'FLOAT'
+        elif dtype == 'datetime64[ns]':
+            column_type = 'DATETIME'
+        else:
+            column_type = 'VARCHAR(MAX)'
+
+        create_table_query += f"{col_escaped} {column_type}, "
+
+    create_table_query = create_table_query.rstrip(", ") + ")"
+    hook.run(create_table_query)
+    print(f"Tabela [{tabela}] criada com sucesso!")
+
+def inserir_parquet_sql_server(parquet_file: str, tabela: str) -> None:
+    df = pd.read_parquet(parquet_file)
+    hook = MsSqlHook(mssql_conn_id='mssql_aws_rds')
+
+    table_exists_query = f"SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = '{tabela}'"
+    result = hook.get_first(table_exists_query)
+
+    if not result:
+        print(f"Tabela [{tabela}] não existe. Criando a tabela...")
+        criar_tabela_sql(df, tabela, hook)
+
+    df.columns = [formatar_nome_coluna(col) for col in df.columns]
+
+    hook.insert_rows(table=tabela, rows=df.values.tolist(), target_fields=df.columns.tolist())
+    print(f"Dados do arquivo Parquet inseridos na tabela [{tabela}] com sucesso!")
+
+def gerar_relatorio_e_inserir_sql(parquet_file: str):
+    tabela_destino_atracacao = "atracacao_fato"
+    tabela_destino_carga = "carga_fato"
+    
+    inserir_parquet_sql_server(parquet_file, tabela_destino_atracacao)
+    inserir_parquet_sql_server(parquet_file, tabela_destino_carga)
+
 dag = DAG(
     'atracacoes_e_cargas',
     start_date=datetime(2025, 2, 15),
-    schedule_interval='@montly',
+    schedule_interval='@monthly',
     catchup=False
 )
 
 descompactacao = TaskGroup("group_descompactacao", dag=dag)
 upload_aws_s3 = TaskGroup("group_upload_aws_s3", dag=dag)
+inserir_aws_rds = TaskGroup("group_upload_aws_rds", dag=dag)
 
 task_descompactar_arquivo_2021 = PythonOperator(
     task_id='descompactar_arquivo_2021',
@@ -96,6 +162,22 @@ task_upload_carga_to_s3 = PythonOperator(
         dag=dag
 )
 
+task_inserir_atracacao_aws_sql = PythonOperator(
+    task_id='inserir_atracacao_sql',
+    python_callable=gerar_relatorio_e_inserir_sql,
+    op_args=['/usr/local/airflow/dags/datalake/business/atracacao.parquet'],
+    task_group=inserir_aws_rds,
+    dag=dag
+)
+
+task_inserir_carga_aws_sql = PythonOperator(
+    task_id='inserir_carga_sql',
+    python_callable=gerar_relatorio_e_inserir_sql,
+    op_args=['/usr/local/airflow/dags/datalake/business/carga.parquet'],
+    task_group=inserir_aws_rds,
+    dag=dag
+)
+
 descompactacao >> task_captura_dados
 task_captura_dados >> task_processamento_de_dados >> task_limpar_e_validar_dados >> task_gerar_relatorio
-task_gerar_relatorio >> upload_aws_s3
+task_gerar_relatorio >> [upload_aws_s3,  inserir_aws_rds]
